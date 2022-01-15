8fac5a52e83fd292fd4d0389f40632cba90cd8ea
Peter B Nguyen <peterbaovietnguyen@gmail.com>
2022-01-15 19:48:08 +1030

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,9 +1,31 @@
-/** An empty service worker! */
+var STATIC_CACHE_CONTAINER = "static_v1"
+var STATIC_FILES = [
+    "/",
+    "/index.js",
+    "/index.css",
+    "https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+]
 
-self.addEventListener (‘fetch’, function(event)
+self.addEventListener('install', function(event){
+    event.waitUntil(
+        caches.open(STATIC_CACHE_CONTAINER)
+        .then(function(cache){
+            cache.addAll(STATIC_FILES)
+        })
+    )
+})
 
-{
+self.addEventListener('activate', function(event){
+    console.log("service worker activated", event)
+})
 
-/** An empty fetch handler! */
-
-});
+self.addEventListener('fetch', function(event){
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(response){
+            if(response){
+                return response
+            } 
+        })
+    )
+})
